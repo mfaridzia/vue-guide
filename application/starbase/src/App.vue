decb5386a6674c730ff9534738abe1990de1756a
mfaridzia <farid19barcelona@gmail.com>
2018-08-31 14:06:15 +0700

    add starbase

---
+++ application/starbase/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h3> {{title}} </h3>
+    <div class="col-md-12">
+      <Character 
+        v-for="(id, index) in initial_ids" 
+        :key="index"
+        :id="id"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Generate Your Team',
+      initial_ids: [1, 9, 12]
+    }
+  },
+  components: {
+    Character
+  }
+}
+</script>
+
+<style>
+body {
+  background: #263238;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+</style>
---
+++ application/starbase/src/components/Character.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-md-4" @click="switchCharacter">
+    <div class="character-card">
+      <div class="card-block">
+        <h4 class="card-title"> {{character.name}} </h4>
+        <p class="card-text"> Height: {{character.height}} </p>
+        <p class="card-text"> Mass: {{character.mass}} </p>
+        <p class="card-text"> Hair Color: {{character.hair_color}} </p>
+        <p class="card-text"> Eye Color: {{character.eye_color}} </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      character: {}
+    }
+  },
+  created() {
+    this.fetchCharacter(this.id)
+  },
+  methods: {
+    fetchCharacter(id) {
+      fetch(`https://swapi.co/api/people/${id}`, {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(resp => this.character = resp)
+    },
+    switchCharacter() {
+      let random_id = Math.floor(Math.random() * 83) + 1
+      this.fetchCharacter(random_id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.character-card {
+  border: 2px solid rgb(9, 110, 102);
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
