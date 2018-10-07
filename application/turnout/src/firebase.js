143a5e92ff60dee373d724e6ba4bab9d48877a67
Muhammad Farid Zia <farid19barcelona@gmail.com>
2018-10-07 22:28:10 +0700

    Update firebase.js

--- application/turnout/src/firebase.js
+++ application/turnout/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAj9HFYr8aYxCYhJV_xe3ER1n47UqKpNiI",
-  authDomain: "turnout-3e1e5.firebaseapp.com",
-  databaseURL: "https://turnout-3e1e5.firebaseio.com",
-  projectId: "turnout-3e1e5",
-  storageBucket: "turnout-3e1e5.appspot.com",
-  messagingSenderId: "230260758712"
+  apiKey: "api-key",
+  authDomain: "auth-domain",
+  databaseURL: "db-url",
+  projectId: "project-id",
+  storageBucket: "storage-bucket",
+  messagingSenderId: "mess-send-id"
 };
 
 export const firebaseApp = firebase.initializeApp(config)
